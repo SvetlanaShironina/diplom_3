2e31faabbb1b554af2b58bea6504ab4dab2910bd
Sveta <Shironin10@gmail.com>
2023-10-14 15:38:53 +0300

    add conf.properties

--- src/test/java/base/CommonBaseTest.java
+++ src/test/java/base/CommonBaseTest.java
@@ -1,5 +1,6 @@
 package base;
 
+import base.org.example.ConfProperties;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
@@ -10,17 +11,17 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.concurrent.TimeUnit;
 
 public class CommonBaseTest {
-        public static final String PAGE_URL = "https://stellarburgers.nomoreparties.site";
-        public WebDriver driver;
-        @Before
-        public void setUp() {
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--remote-allow-origins=*");
-                System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\bin\\chromedriver\\chromedriver.exe");
-                driver = new ChromeDriver(options);
-                driver.manage().window().maximize();
-                WebDriverManager.chromedriver().setup();
-                driver.get(PAGE_URL);
+    public static final String PAGE_URL = "https://stellarburgers.nomoreparties.site";
+    public WebDriver driver;
+    @Before
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        WebDriverManager.chromedriver().setup();
+        driver.get(PAGE_URL);
 
 //        WebDriverManager.edgedriver().setup();
 //        EdgeOptions options = new EdgeOptions();
